a8fb776619c0dc84a41c3fb46fb9ca43d6d67c17
Udith Sandaruwan <developer.udithsandaruwan@gmail.com>
2024-07-27 13:44:29 +0530

    Update settings.py

--- arcsharing/settings.py
+++ arcsharing/settings.py
@@ -131,3 +131,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if os.getcwd() == '/app':
+    DEBUG = False
